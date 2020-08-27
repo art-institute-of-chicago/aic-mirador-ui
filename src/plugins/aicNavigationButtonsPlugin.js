68087ff2f200d2d2fcff82e093499cdc76aa4c02
surreal8 <tinashah8@gmail.com>
2020-08-27 18:54:23 -0500

    Customize navigation buttons [WEB-1814]

--- src/plugins/aicNavigationButtonsPlugin.js
+++ src/plugins/aicNavigationButtonsPlugin.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Button from '@material-ui/core/Button';
 import SvgIcon from '@material-ui/core/SvgIcon';
-//import { getViewer } from 'mirador/dist/es/src/state/selectors';
+import IconButton from '@material-ui/core/IconButton';
 import * as actions from 'mirador/dist/es/src/state/actions';
 import {
   getSequenceViewingDirection,
   getNextCanvasGrouping,
   getPreviousCanvasGrouping,
 } from 'mirador/dist/es/src/state/selectors';
-import ns from 'mirador/dist/es/src/config/css-ns';
+import { withStyles } from '@material-ui/core/styles';
 
 //custom icons
 const PrevIcon = (props) => (
   <SvgIcon {...props}>
-    <svg viewBox="0 0 24 24" id="icon--arrow--24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9 5.55c1.915 2.632 3.343 4.542 4.57 6.45-1.227 1.908-2.655 3.817-4.57 6.45l1.2.55c2.229-2.46 4.143-4.57 5.8-6.71v-.58C14.342 9.57 12.429 7.46 10.2 5z" fill="currentColor">
+    <svg viewBox='0 0 24 24' id='icon--arrow--24' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M9 5.55c1.915 2.632 3.343 4.542 4.57 6.45-1.227 1.908-2.655 3.817-4.57 6.45l1.2.55c2.229-2.46 4.143-4.57 5.8-6.71v-.58C14.342 9.57 12.429 7.46 10.2 5z' fill='currentColor'>
       </path>
     </svg>
   </SvgIcon>
@@ -23,55 +21,48 @@ const PrevIcon = (props) => (
 
 const NextIcon = (props) => (
   <SvgIcon {...props}>
-    <svg viewBox="0 0 24 24" id="icon--arrow--24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9 5.55c1.915 2.632 3.343 4.542 4.57 6.45-1.227 1.908-2.655 3.817-4.57 6.45l1.2.55c2.229-2.46 4.143-4.57 5.8-6.71v-.58C14.342 9.57 12.429 7.46 10.2 5z" fill="currentColor">
+    <svg viewBox='0 0 24 24' id='icon--arrow--24' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M9 5.55c1.915 2.632 3.343 4.542 4.57 6.45-1.227 1.908-2.655 3.817-4.57 6.45l1.2.55c2.229-2.46 4.143-4.57 5.8-6.71v-.58C14.342 9.57 12.429 7.46 10.2 5z' fill='currentColor'>
       </path>
     </svg>
   </SvgIcon>
 );
 
+const CustomButtonPrevious = withStyles({
+  root: {
+    transform: 'scale(1.5) rotate(180deg)',
+    left: '5%',
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.0)',
+    },
+  },
+})(IconButton);
+
+const CustomButtonNext = withStyles({
+  root: {
+    transform: 'scale(1.5)',
+    right: '-90%',
+    '&:hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.0)',
+    },
+  },
+})(IconButton);
+
 class NavigationButtonsPlugin extends Component {
   render() {
     const {
-      hasNextCanvas, hasPreviousCanvas, setNextCanvas, setPreviousCanvas, t, viewingDirection,
+      hasNextCanvas, hasPreviousCanvas, setNextCanvas, setPreviousCanvas, t,
     } = this.props;
 
-    let htmlDir = 'ltr';
-    let previousIconStyle = {};
-    let nextIconStyle = {};
-    switch (viewingDirection) {
-      case 'top-to-bottom':
-        previousIconStyle = { transform: 'rotate(270deg)' };
-        nextIconStyle = { transform: 'rotate(90deg)' };
-        break;
-      case 'bottom-to-top':
-        previousIconStyle = { transform: 'rotate(90deg)' };
-        nextIconStyle = { transform: 'rotate(270deg)' };
-        break;
-      case 'right-to-left':
-        htmlDir = 'rtl';
-        previousIconStyle = {};
-        nextIconStyle = { transform: 'rotate(180deg)' };
-        break;
-      default:
-        previousIconStyle = { transform: 'rotate(180deg)', position: 'relative', left: 5};
-        nextIconStyle = {};
-    }
-
-    // to do: convert ButtonGroup and Buttons to ToggleButtonGroup
     return (
-      <div style={{position: 'absolute', bottom: 150, right: 100, zIndex:500}}>
-        <ButtonGroup variant="contained" color="primary">
-          <Button startIcon={<PrevIcon  style={previousIconStyle} />} aria-label={t('previousCanvas')}
-          className={ns('previous-canvas-button')}
-          disabled={!hasPreviousCanvas}
-          onClick={() => { hasPreviousCanvas && setPreviousCanvas(); } }></Button>
-          <Button startIcon={<NextIcon style={nextIconStyle} />} aria-label={t('nextCanvas')}
-          className={ns('next-canvas-button')}
-          disabled={!hasNextCanvas}
-          onClick={() => { hasNextCanvas && setNextCanvas(); }}></Button>
-        </ButtonGroup>
-    </div>
+      <div style={{position: 'absolute', bottom: '50%', zIndex:500, width:'100%'}}>
+        <CustomButtonPrevious aria-label='previous canvas'  disabled={!hasPreviousCanvas} onClick={() => { hasPreviousCanvas && setPreviousCanvas(); }}>
+          <PrevIcon />
+        </CustomButtonPrevious>
+        <CustomButtonNext aria-label='next canvas' disabled={!hasNextCanvas} onClick={() => { hasNextCanvas && setNextCanvas(); }}>
+          <NextIcon />
+        </CustomButtonNext>
+      </div>
     );
   }
 }
