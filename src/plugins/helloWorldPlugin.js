a6855ff89c4435f7bdc526c092e4c249c45864d4
surreal8 <tinashah8@gmail.com>
2021-02-01 13:58:21 -0600

    Add caveats to view example plugin

--- src/plugins/helloWorldPlugin.js
+++ src/plugins/helloWorldPlugin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+// 'allowTopMenuButton' should be set to 'true' to see this example. Also may need to remove AIC customizations to view.
 class helloWorld extends Component {
   render() {
     return (
